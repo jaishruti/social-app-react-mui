2ade427dfaa48dc635678b1a37c0f9aee39ac96d
Shruti Jaiswal <shrutijaiswal223@gmail.com>
2023-04-08 10:08:40 +0000

    ThemeProvider added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
 import { useState } from "react";
 import { Add, Settings } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import { Button, createTheme } from "@mui/material";
 // import { styled } from "@emotion/styled";
 import { Box, Stack } from "@mui/material";
 import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
 import Feed from "./components/Feed";
 import Navbar from "./components/Navbar";
+import ThemeProvider from "@mui/material";
+
 
 function App() {
   const [count, setCount] = useState(0);
+  const [mode,setMode] = useState("light")
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  }) 
 
   return (
     // <Box>
     //   <Stack direction={"row"} spacing={2} >
+    <ThemeProvider theme={darktheme}>
     <Box>
       <Navbar/>
-      <Stack spacing={2} direction={"row"} justifyContent={"space-between"}>
+      <Stack direction={"row"} spacing={2} justifyContent={"space-between"}>
         <Sidebar />
         <Feed />
         <Rightbar />
       </Stack>
     </Box>
+    </ThemeProvider>
     //   {/* </Stack> */}
     // {/* </Box> */}
   );
